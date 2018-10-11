2590e5997886be0a364b6ecafdfe39c6a7ee61ef
Karolin Laas <karolin.laas@khk.ee>
2018-10-11 12:50:06 +0300

    Liigutatud tehte loogika meetodisse, lisatud konditsionaalne renderdamine, lisatud stiil.

--- src/components/Konverter.vue
+++ src/components/Konverter.vue
@@ -3,18 +3,18 @@
     <h1>Kaalu teisendamine</h1>
     <h4>Sisesta kaal kilogrammides:</h4>
     <input type="number" min="0" v-model="kg">
-    <table>
+    <table v-if="kg !== ''">
       <tr>
         <td>Grammid</td>
-        <td>{{ `${kg * 1000} g` }}</td>
+        <td class="right">{{ convertKg(1000) + ' g' }}</td>
       </tr>
       <tr>
         <td>Naelad</td>
-        <td>{{ `${(kg * 2.20462262).toFixed(2)} lb` }}</td>
+        <td class="right">{{ convertKg(2.20462262) + ' lb' }}</td>
       </tr>
       <tr>
         <td>Untsid</td>
-        <td>{{ `${(kg * 35.2739619).toFixed(2)} oz` }}</td>
+        <td class="right">{{ convertKg(35.2739619) + ' oz' }}</td>
       </tr>
     </table>
   </div>
@@ -25,15 +25,37 @@ export default {
   name: 'Konverter',
   data () {
     return {
-       kg: 0
+       kg: ''
     }
   },
   methods: {
-   
+    convertKg(multiplier) { 
+      return (this.kg !== 0) ? (this.kg * multiplier).toFixed(2) : 0
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  div {
+    text-align: center;
+    padding: 0;
+	box-sizing: border-box;
+	font-family: 'Montserrat', sans-serif;
+    margin-top: 25vh;
+  }
+  table {
+    margin-left: auto;
+    margin-right: auto;
+    text-align: left;
+    font-weight: bolder; 
+  }
+  td {
+    padding-top: 7px;
+  }
+  .right {
+    text-align: right;
+    padding-left: 20px;
+  }
 </style>
